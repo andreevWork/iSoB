ac0c5e7f1b3e273ee73d84f5b3e10d65353545c5
vandreev <vandreevwork94@gmail.com>
2016-06-19 06:09:47 +0300

    перенес данные в JSON. Перевел все на jade.

--- index.js
+++ index.js
@@ -2,76 +2,39 @@ var express = require('express'),
 	fs = require('fs'),
 	favicon = require('serve-favicon'),
 	path = require('path'),
+	jade = require('jade'),
 	server = express(),
 
-/*
-	*	Вместо базы или отдельного файла. Ключи объекта - имена папок с проектом.
-	*/
-	projects_json = {
-		knockout_todo : {
-			title : 'Todo приложение с использованием KnockoutJS.',
-			tools : ['KnockoutJS', 'Bootstrap 3', 'ES2015+']
-		},
-		react_redux_shop : {
-			title : 'Галлерея магазина с фильтрацией и сортировкой',
-			tools : ['React', 'Redux', 'Bootstrap 3', 'ES2015+']
-		}
-	},
+	projects_json = JSON.parse(fs.readFileSync('projects.json', 'utf8')),
+	projects = Object.keys(projects_json);
 
-/*
-	*	Смотрит в текущую директорию, и забирает все папки с проектами, отсеивая файлы и не нужные папки, возьмет только те проекты, которые описаны в объекте выше.
-	*/
-	Projects = fs.readdirSync(__dirname).filter(function(file){
-		return fs.statSync(path.join(__dirname, file)).isDirectory() && !!projects_json[file];
-	}),
-
-	CommonHtml = fs.readFileSync(getPath('common.html'), 'utf8'),
-
-	IndexHtml = fs.readFileSync(getPath('index.html'), 'utf8')
-		.replace('<--list-->', getListProjects().join('')),
-
-	html;
+server.set('view engine', 'jade');
+server.set('views', './views');
 
 server.use('/public', express.static(__dirname + '/public'));
 server.use(favicon(__dirname + '/public/favicon.png'));
 
-Projects.forEach(function(path){
+projects.forEach(function(path){
 	server.use('/' + path + '/public', express.static(__dirname + '/' + path + '/public'));
 });
 
-server.get('/', function(req, res){ 
-	res.end(IndexHtml);
+
+server.get('/', function(req, res){
+	res.render('index', {
+        projects: projects_json,
+        path_to_style: 'public/css/main.css'
+    });
 });
 
 server.get('/:project', function(req, res){
 	var {project} = req.params;
-	html = changeJsScriptath(getHtml(fs.readFileSync(getPath(project + '/index.html'), 'utf8'), projects_json[project].title), project);
-	res.end(html);
+    res.render(project, {
+        title: projects_json[project].title,
+        path_to_style: project + '/public/main.css',
+        path_to_script: project + '/public/main.js'
+    });
 });
 
 server.listen(3001, function(){
 	console.log('Сервер запущен...');
 });
-
-function getHtml(body, breadcrumb) {
-	breadcrumb = breadcrumb || '';
-	return CommonHtml.replace('<-- body -->', body).replace('<-- breadcrumb -->', breadcrumb);
-}
-
-function changeJsScriptath(html, path) {
-	return html.replace('<-- path -->', path);
-}
-
-function getListProjects() {
-	return Projects.map(function(project){
-		return '<a href="/' + project + '" class="list-group-item"><b>' + 
-			projects_json[project].title + 
-			'</b><br /><small>Инструменты: ' + 
-			projects_json[project].tools.join(', ') + 
-			'.</small></a>';
-	});
-}
-
-function getPath(file) {
-	return path.join(__dirname, file)
-}
--- knockout_todo/webpack.config.js
+++ knockout_todo/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	entry : './js/app.js',
 	output : {
-		filename : 'index.js',
+		filename : 'main.js',
 		path : __dirname + '/public'
 	},
 	module: {
